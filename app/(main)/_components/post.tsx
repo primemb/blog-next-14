b61070d1865ce097c16b2b3070050e78fed32b32
Mohammad Reza <mo.behzad34@gmail.com>
2023-11-03 15:05:22 +0330

    add delete post button ui

--- app/(main)/_components/post.tsx
+++ app/(main)/_components/post.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { IPost } from "@/data/posts";
 
 import {
@@ -11,15 +13,50 @@ import {
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { MoreHorizontal, Trash } from "lucide-react";
+import {
+  DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import useAuth from "@/hooks/use-auth";
 
 interface PostProps {
   post: IPost;
 }
 
 const Post = ({ post }: PostProps) => {
+  const { isLogin } = useAuth();
   return (
-    <Card className="bg-white dark:bg-primary/5 text-black dark:text-white border-none p-4 mb-4">
+    <Card className=" bg-white dark:bg-primary/5 text-black dark:text-white border-none p-4 mb-4">
       <CardHeader>
+        {isLogin && (
+          <div className="flex flex-row-reverse w-full">
+            <DropdownMenu>
+              <DropdownMenuTrigger className="focus-visible:outline-none">
+                <div
+                  role="button"
+                  className="group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 focus-visible:outline-none"
+                >
+                  <MoreHorizontal className="text-muted-foreground" />
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                className="w-60"
+                align="center"
+                side="left"
+                forceMount
+              >
+                <DropdownMenuItem onClick={() => {}}>
+                  <Trash className="h-4 w-4 mr-2" />
+                  Delete
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        )}
+
         <CardTitle>{post.title}</CardTitle>
         <CardDescription>Author : {post.author}</CardDescription>
       </CardHeader>
--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -17,8 +17,12 @@ export function ModeToggle() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button className="dark:bg-primary/5" variant="outline" size="icon">
+      <DropdownMenuTrigger className="focus-visible:outline-none" asChild>
+        <Button
+          className="dark:bg-primary/5 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
+          variant="outline"
+          size="icon"
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
