478367d543339cef0004f4be94629311807b7a00
Mohammad Reza <mo.behzad34@gmail.com>
2023-11-03 13:53:08 +0330

    redirect if user login on auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,14 @@
+import { getCookies } from "next-client-cookies/server";
+import { redirect } from "next/navigation";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  const cookies = getCookies();
+  const token = cookies.get("token");
+
+  if (token) {
+    redirect("/");
+  }
+
   return (
     <div className="w-full min-h-full dark:bg-[#1F1F1F] bg-gray-100">
       {children}
