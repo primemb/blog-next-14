7a7dfd17acaf8337d56d790dc0289b38bb45628e
Mohammad Reza <mo.behzad34@gmail.com>
2023-11-04 11:33:13 +0330

    add footer

---
+++ app/(main)/_components/footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="xl:px-60">
+      <div className="flex min-h-[2rem] rounded-t-md bg-white dark:bg-primary/5 items-center justify-center">
+        <p>
+          Built with ❤️ by{" "}
+          <a className="text-indigo-400" href="https://mbehzadfar.dev">
+            M.B
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,12 @@
+import Footer from "./_components/footer";
 import Navbar from "./_components/navbar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col w-full min-h-full dark:bg-[#1F1F1F] bg-gray-100 ">
       <Navbar />
-      <main className="h-full w-full px-2 xl:px-60">{children}</main>
+      <main className="h-full flex-1 w-full px-2 xl:px-60">{children}</main>
+      <Footer />
     </div>
   );
 };
